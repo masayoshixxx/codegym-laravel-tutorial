1832ea750da5586267a5a7aaeedab897c04494a3
masayoshixxx <satomasayoshi.xxx@gmail.com>
2021-11-27 16:43:50 +0900

    detailに関する項目をテーブルに追加

--- application/database/migrations/2021_03_27_145301_create_tasks_table.php
+++ application/database/migrations/2021_03_27_145301_create_tasks_table.php
@@ -50,6 +50,7 @@ class CreateTasksTable extends Migration
             $table->foreignId('project_id')->constrained('projects');
             $table->string('name');
             $table->foreignId('task_kind_id')->constrained('task_kinds');
+            $table->text('detail')->nullable();
             $table->foreignId('task_status_id')->constrained('task_statuses');
             $table->foreignId('created_user_id')->constrained('users');
             $table->foreignId('updated_user_id')->nullable()->constrained('users');
